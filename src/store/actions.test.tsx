01d6a2288bf5931af45e46fec2a2ae21dd9460b9
Jonathan Undelikwo <jundelikwo@gmail.com>
2022-10-30 19:17:23 +0100

    Added fetchCoins test

--- src/store/actions.test.tsx
+++ src/store/actions.test.tsx
@@ -2,13 +2,10 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import App from '../App';
 
-import {render, waitFor} from '@testing-library/react-native';
+import {fetchCoins} from './actions';
 
 const data = {
   success: true,
@@ -18,14 +15,14 @@ const data = {
   },
 };
 
-describe('App', () => {
-  it('should render successfully', () => {
+describe('fetchCoins', () => {
+  it('should call the api', async () => {
     const mock = new MockAdapter(axios);
     mock
       .onGet('https://staging-biz.coinprofile.co/v3/currency/rate')
       .reply(200, data);
 
-    render(<App />);
-    waitFor(() => null);
+    await fetchCoins(res => res);
+    expect(mock.history.get.length).toEqual(1);
   });
 });
