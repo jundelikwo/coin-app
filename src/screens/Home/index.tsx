ddb36d9ad071b919ca6d3f752523e324f6a115a6
Jonathan Undelikwo <jundelikwo@gmail.com>
2022-10-30 20:12:37 +0100

    Added CoinDetails to Home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,31 @@
-import React, {Fragment, useMemo, useState} from 'react';
+import React, {Fragment, useCallback, useMemo, useRef, useState} from 'react';
 import {SafeAreaView, View} from 'react-native';
 import SearchInput from 'src/components/SearchInput';
 import Header from 'src/components/Header';
 import CoinList from 'src/components/CoinList';
 import {RateType} from 'src/store/actions';
 import styles from './styles';
+import CoinDetails from '../../components/CoinDetails';
 
 export interface HomeProps {
   rates: RateType;
 }
 
 export const Home = ({rates}: HomeProps) => {
+  const bottomSheet = useRef<any>(null);
   const [searchText, setSearchText] = useState('');
+  const [selectedCoin, setSelectedCoin] = useState('');
   const ratesKeys = useMemo(() => {
     const formattedSearchText = searchText.trim().toLowerCase();
     return Object.keys(rates).filter(
       rate => rate.toLowerCase().indexOf(formattedSearchText) >= 0,
     );
   }, [rates, searchText]);
+
+  const handleCoinSelect = useCallback((coin: string) => {
+    setSelectedCoin(coin);
+    bottomSheet.current.open();
+  }, []);
 
   return (
     <Fragment>
@@ -33,7 +41,12 @@ export const Home = ({rates}: HomeProps) => {
             onChangeText={setSearchText}
           />
         </View>
-        <CoinList coins={ratesKeys} />
+        <CoinList coins={ratesKeys} onCoinSelect={handleCoinSelect} />
+        <CoinDetails
+          forwardedRef={bottomSheet}
+          coin={selectedCoin}
+          rate={rates[selectedCoin]?.rate}
+        />
       </SafeAreaView>
     </Fragment>
   );
